fcfd94cd7d8248a3a7f3be5552a0323e41aff032
quantumblockz <quarkbread@protonmail.com>
2018-08-10 03:54:42 -0400

    Added JSON data instead of headers

--- floodtxs.py
+++ floodtxs.py
@@ -5,16 +5,17 @@ import sched
 
 url = 'http://localhost:9901/api/v1/data/transactions'
 
-headers = {'Content-Type': 'application/json', 'origin':
-           '58BF325AF01CCC78265EB715C1EB10EEA455905D4B50C2AC6541950D97DF8607', 'destination':
-           '5CA1EEF9AA50625F3B7AC637D35655174CAA2C4FAB559B294D6E7C924C9AA6D4', 'passphrase':
-           'root_password', 'args': {'type': 'BIGINT', 'value': '1'}}
+headers = {'Content-Type': 'application/json'}
+
+json = {'origin': '58BF325AF01CCC78265EB715C1EB10EEA455905D4B50C2AC6541950D97DF8607', 'destination':
+        '5CA1EEF9AA50625F3B7AC637D35655174CAA2C4FAB559B294D6E7C924C9AA6D4', 'passphrase':
+        'root_password', 'args': {'type': 'BIGINT', 'value': '1'}}
 
 #Established base time for repeating tasks
 s = sched.scheduler(time.time, time.sleep)
 
 def floodTxs():
-    r = requests.post(url, headers=headers)
+    r = requests.post(url, headers=headers, json=json)
     print(r.text)
     s.enter(1, 1, floodTxs, ()) #Specify number of seconds in first parameter
     s.run()
